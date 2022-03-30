4ee4818ada33c21e95bd61f96404e93a68a612e9
RUTER <rutkowskimichal03@gmail.com>
2022-03-30 12:20:56 +0200

    full code

--- newPatient.php
+++ newPatient.php
@@ -1,13 +1,25 @@
 <?php
 if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])){
+
+    $db = new mysqli("localhost", "root", "", "med");
+    $q = $db->prepare("INSERT INTO patient VALUES (NULL, ?, ?, ? , ?)");
+    $q->bind_param("ssss", $_REQUEST['firstName'], $_REQUEST['lastName'],
+                            $_REQUEST['phone'], $_REQUEST['pesel']);
+    if($q->execute()) {
+        echo "Pacjent dodany do bazy";
+    }
+
+
 }else {
     echo '
     <form action="newPatient.php" method="post">
     <label for="firstName">Imię:</label>
-    <input type="text" name="firstName" id ="firstName">
+    <input type="text" name="firstName" id="firstName">
     <label for="lastName">Nazwisko:</label>
     <input type="text" name="lastName" id="lastName">
-    <label for="pesel">Numer Pesel</label>
+    <label for="phone">Numer telefonu:</label>
+    <input type="text" name="phone" id="phone">
+    <label for="pesel">Numer PESEL</label>
     <input type="text" name="pesel" id="pesel">
     <input type="submit" value="Zapisz">
     </form>
@@ -15,5 +27,5 @@ if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])){
 }
 
 ?>
-<input type="text" name="" id="">
+
 
